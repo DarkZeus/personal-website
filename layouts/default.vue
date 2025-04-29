d96a077004204f337a49c396720e778afd5bf962
Serhii Resnyanskyi <serhii.resnyanskyi@gmail.com>
2025-04-29 14:30:38 +0300

    Fix imports

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,6 @@
 </template>
 
 <script setup>
-import Navbar from '~/components/layout/navbar.vue'
-import Footer from '~/components/layout/footer.vue'
+import Navbar from '~/components/layout/Navbar.vue'
+import Footer from '~/components/layout/Footer.vue'
 </script> 
